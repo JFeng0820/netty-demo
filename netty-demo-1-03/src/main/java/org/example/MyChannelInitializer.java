fb1e9aa22b934d58d6861fa92471bb3e26720830
Jfeng <1029533042@qq.com>
2024-01-17 00:17:31 +0800

    fix： 1、补充提交遗漏文件

--- netty-demo-1-03/src/main/java/org/example/MyChannelInitializer.java
+++ netty-demo-1-03/src/main/java/org/example/MyChannelInitializer.java
@@ -2,13 +2,21 @@ package org.example;
 
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
-import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.Charset;
 
 public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
         // 基于换行符号
-        ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
-
+//        ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        // 基于最大长度
+        ch.pipeline().addLast(new FixedLengthFrameDecoder(4));
+        // 解码转String
+        ch.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+        // 在管道中添加我们自己的接收数据实现方法
+        ch.pipeline().addLast(new MyServerHandler());
     }
 }
---
+++ netty-demo-1-04/src/main/java/org/example/MyChannelInitializer.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+import java.nio.charset.Charset;
+
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        channel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+        channel.pipeline().addLast(new StringDecoder(Charset.forName("GBK")));
+        channel.pipeline().addLast(new MyServerHandler());
+    }
+}
